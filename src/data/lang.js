7b94f6b2cb4584e9fed43016f7e151678129bf8f
zalfalqyn <73231546+zalfalqyn@users.noreply.github.com>
2023-08-09 21:54:54 +0700

    Update link cv

--- src/data/lang.js
+++ src/data/lang.js
@@ -12,7 +12,7 @@ const lang = {
     description:
       "I love to solve problems by creating effective solutions that balance user and business perfectly",
     resume: {
-      link: "ristek.link/Resume-ZalfaLA",
+      link: "ristek.link/CVZalfaLuqyanaAkifah",
       label: "Download CV",
     },
   },
